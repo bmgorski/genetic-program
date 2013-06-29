641eafb11886cd9fd4cccfd7be7d61e9ce222fcb
bmgorski <bmgorski@bmgorski-PC>
2013-06-29 11:47:09 -0500

    Added a levelOrder function to BinaryMathTree, this should be useful to find nodes near the end of the tree in order to do mutations and crossovers on those items

--- GeneticProgram/src/main/java/com/genetic/program/tree/BinaryMathTree.java
+++ GeneticProgram/src/main/java/com/genetic/program/tree/BinaryMathTree.java
@@ -2,7 +2,11 @@ package com.genetic.program.tree;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+
+import org.hamcrest.core.IsInstanceOf;
 
 public class BinaryMathTree {
 	private TreeNode rootNode;
@@ -26,5 +30,27 @@ public class BinaryMathTree {
 	
 	public int size(){
 		return rootNode.getNumberOfChildren();
+	}
+	
+	public List<TreeNode> levelorder(){
+		List<TreeNode> levelOrder = new ArrayList<TreeNode>();
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.add(rootNode);
+		
+		while(!queue.isEmpty()){
+		    TreeNode treeNode = queue.poll();
+		    levelOrder.add(treeNode);
+		    if(treeNode instanceof Operator){
+	    	    Operator operator = (Operator)treeNode;
+	    	    if(operator.getLeftNode() != null){
+	    	    	queue.add((TreeNode)operator.getLeftNode());
+	    	    }
+	    	    if(operator.getLeftNode() != null){
+	    	    	queue.add((TreeNode)operator.getLeftNode());
+	    	    }
+		    }
+		}
+		
+		return levelOrder;
 	}
 }
